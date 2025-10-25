5fcc2d0d4b3d870ce4e60c43b56d32ef53194197
24f2001869 <24f2001869@ds.study.iitm.ac.in>
2025-10-25 17:10:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>GitHub Codespaces</title>
-    </head>
-    <body>
-        <h1>GitHub Codespaces Demo</h1>
-        <p>Email: 24f2001869@ds.study.iitm.ac.in</p>
-    </body>
-    </html>
-    """
-
-@app.get("/plain")
-async def plain_text():
-    return "24f2001869@ds.study.iitm.ac.in"
-
-@app.get("/json")
-async def json_response():
-    return {"email": "24f2001869@ds.study.iitm.ac.in"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get("/")
+def read_root():
+    return {"email": "24f2001869@ds.study.iitm.ac.in", "message": "Hello from Codespaces!"}
